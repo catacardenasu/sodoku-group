46cf9556bfe2f41a092d640bd7886d7138a93c61
isatejlle <101947521+isatejlle@users.noreply.github.com>
2025-12-01 18:24:36 -0500

    updated main file

--- sudoku.py
+++ sudoku.py
@@ -3,16 +3,36 @@ import pygame
 def main():
     try:
         pygame.init()
-        screen = pygame.display.set_mode((500, 700))
+        screen = pygame.display.set_mode((513, 680))
+        pygame.display.set_caption("Sudoku")
+        background_image = pygame.image.load("sudoku_pic.webp")
+        background_image = pygame.transform.scale(background_image, (513, 680))
         running = True
         while running:
-            screen.fill("white")
-            for i in range(9):
-                pygame.draw.line(screen, "black", (i * 32, 0), (i * 32, 512))
-            for i in range(9):
-                pygame.draw.line(screen, "black", (0, i * 32), (640, i * 32))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
+
+        def in_progress():
+            pygame.init()
+            screen = pygame.display.set_mode((513, 680))
+            running = True
+            clock = pygame.time.Clock()
+            while True:
+                # Process player inputs.
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        raise SystemExit
+                screen.fill("purple")
+
+                pygame.display.flip()
+                clock.tick(60)
+
     finally:
-        pygame.quit()
+        raise SystemExit
 
 if __name__ == '__main__':
     main()
